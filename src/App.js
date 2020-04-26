b666a52c4143359ece5f983a770029b1145d4b7c
aleksi.siponen <aleksi.siponen@gmail.com>
2020-04-26 22:42:26 +0300

    muutin hommia

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          nyt tähän on lisätty jotain
         </p>
         <a
           className="App-link"
